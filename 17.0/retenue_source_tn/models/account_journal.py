7afe5c2442b66048410ac19995736e6bf12c5f53
AKREM.KHELIFI <akremkhelifi07@gmail.com>
2026-07-19 01:16:13 +0000

    retenue_source_tn 17.0.1.2.1 — screenshots demo Merkago

--- 17.0/retenue_source_tn/models/account_journal.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class AccountJournal(models.Model):
-    _inherit = "account.journal"
-
-    rs_withholding_sales = fields.Boolean(string="RAS Clients")
-    rs_withholding_purchases = fields.Boolean(string="RAS Fournisseurs")
